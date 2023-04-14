4c9d4700bd197cf3a6e3d3f41b71f17512f0d1d6
Arya-Ebrahimi <aryaebrahimi3000@mail.um.ac.ir>
2023-04-14 22:30:16 +0330

    add the report

--- MiniProject1/test.py
+++ MiniProject1/test.py
@@ -26,7 +26,8 @@ class Wrapper(gym.Wrapper):
 def play(mode, gamma=0.9, is_slippery=False, step_reward=0.0, hole_reward=0.0):
     env = gym.make("FrozenLake-v1", is_slippery=is_slippery, render_mode='rgb_array')
     env = Wrapper(env=env, step_reward=step_reward, hole_reward=hole_reward)
-    env = record_video.RecordVideo(env, video_folder='runs', name_prefix=mode)
+    name = mode+',gamma:'+str(gamma)+',is_slippery:'+str(is_slippery)+',step_reward:'+str(step_reward)+',hole_reward:'+str(hole_reward)
+    env = record_video.RecordVideo(env, video_folder='runs', name_prefix=name)
     if mode == 'policy_iteration':
         V, pi = policy_iteration(env.P, env.observation_space.n, env.action_space.n, gamma=gamma, tol=1e-4)
     else:
@@ -36,7 +37,9 @@ def play(mode, gamma=0.9, is_slippery=False, step_reward=0.0, hole_reward=0.0):
     state, _ = env.reset()
     env.render()
     total_reward = 0
+    steps = 0
     while not done:
+        steps+=1
         action = np.argmax(pi[state])
         next_state, reward, terminated, truncated, info = env.step(action)
         done = terminated or truncated
@@ -46,7 +49,38 @@ def play(mode, gamma=0.9, is_slippery=False, step_reward=0.0, hole_reward=0.0):
     env.close()
     print('total reward is: ', total_reward)
     
-if __name__ == '__main__':
+    return name, steps, total_reward
     
-    play(mode='policy_iteration', gamma=0.9, is_slippery=True, step_reward=-0.05, hole_reward=-2.0)
-    play(mode='value_iteration', gamma=0.9, is_slippery=True, step_reward=-0.05, hole_reward=-2.0)
+if __name__ == '__main__':
+    total_steps = []
+    total_steps.append(play(mode='policy_iteration', gamma=0.0))
+    total_steps.append(play(mode='value_iteration', gamma=0.0))
+    total_steps.append(play(mode='policy_iteration', gamma=0.9))
+    total_steps.append(play(mode='value_iteration', gamma=0.9))
+    total_steps.append(play(mode='policy_iteration', gamma=1.0))
+    total_steps.append(play(mode='value_iteration', gamma=1.0))
+    total_steps.append(play(mode='policy_iteration', gamma=0.9, is_slippery=True))
+    total_steps.append(play(mode='value_iteration', gamma=0.9, is_slippery=True))
+    
+    total_steps.append(play(mode='policy_iteration', gamma=0.9, is_slippery=False, step_reward=-0.05))
+    total_steps.append(play(mode='value_iteration', gamma=0.9, is_slippery=False, step_reward=-0.05))
+    
+    total_steps.append(play(mode='policy_iteration', gamma=0.9, is_slippery=True, step_reward=-0.05))
+    total_steps.append(play(mode='value_iteration', gamma=0.9, is_slippery=True, step_reward=-0.05))
+    
+    total_steps.append(play(mode='policy_iteration', gamma=0.9, is_slippery=False, hole_reward=-2.0))
+    total_steps.append(play(mode='value_iteration', gamma=0.9, is_slippery=False, hole_reward=-2.0))
+    
+        
+    total_steps.append(play(mode='policy_iteration', gamma=0.9, is_slippery=True, hole_reward=-2.0))
+    total_steps.append(play(mode='value_iteration', gamma=0.9, is_slippery=True, hole_reward=-2.0))
+    
+    total_steps.append(play(mode='policy_iteration', gamma=0.9, is_slippery=False, step_reward=-0.05, hole_reward=-2.0))
+    total_steps.append(play(mode='value_iteration', gamma=0.9, is_slippery=False, step_reward=-0.05, hole_reward=-2.0))
+    
+    total_steps.append(play(mode='policy_iteration', gamma=0.9, is_slippery=True, step_reward=-0.05, hole_reward=-2.0))
+    total_steps.append(play(mode='value_iteration', gamma=0.9, is_slippery=True, step_reward=-0.05, hole_reward=-2.0))
+    
+    
+    for i in total_steps:
+        print(i)
